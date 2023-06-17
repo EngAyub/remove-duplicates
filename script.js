dc373682c96706d107a83d1c53fc04d1f18f65d5
ENG AYUUB <moztaphem9@gmal.com>
2023-06-17 20:27:25 +0300

    Remove-duplicates done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+
+function removeDuplicates(AK){
+    return AK.filter((dupli, index)=>{
+        return (AK.indexOf(dupli) === index);
+    })
+}console.log(removeDuplicates([4,7,6,27,4,7,27,2,9,8,1,8]));
+console.log(removeDuplicates(["jamac", "Ayoub", "Ayoub"]));
+console.log(removeDuplicates(["carab", "Ahmed", "Ahmed"]));
